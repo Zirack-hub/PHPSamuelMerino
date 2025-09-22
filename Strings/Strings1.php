fcd34d66a4534b4c1caeaa30d2acee91a6b240aa
Samuel <s.merino.prado@gmail.com>
2025-09-22 10:02:16 +0200

    Ejercicio 1 terminado

--- Strings/Strings1.php
+++ Strings/Strings1.php
@@ -2,13 +2,26 @@
 <HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
 <BODY>
 <?php
-$ip="192.18.16.204";
-$byte1= (int)substr($ip,0,strpos($ip,"."));
-$byte2 = (int)substr($ip,strpos($ip,".")+1,strpos($ip,".",strpos($ip,".")));
-$byte3 = (int)substr($ip,strpos($ip,".")+1,strpos($ip,".",strpos($ip,".")));
+$ip = "192.18.16.204";
 
-echo $byte1 . "<br>";
-echo $byte2 . "<br>";
+
+$p1 = strpos($ip, ".");                  
+$p2 = strpos($ip, ".", $p1 + 1);         
+$p3 = strpos($ip, ".", $p2 + 1);         
+
+$byte1 = (int)substr($ip, 0, $p1);                
+$byte2 = (int)substr($ip, $p1 + 1, $p2 - $p1 - 1);
+$byte3 = (int)substr($ip, $p2 + 1, $p3 - $p2 - 1);
+$byte4 = (int)substr($ip, $p3 + 1);               
+
+
+$bin1 = sprintf("%08b", $byte1);
+$bin2 = sprintf("%08b", $byte2);
+$bin3 = sprintf("%08b", $byte3);
+$bin4 = sprintf("%08b", $byte4);
+
+
+echo "IP $ip en binario es $bin1.$bin2.$bin3.$bin4";
 ?>
 </BODY>
 </HTML>
