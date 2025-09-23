a1d7dc08d35efd8a84db877bd998710eddef3c19
Samuel <s.merino.prado@gmail.com>
2025-09-23 11:10:33 +0200

    Terminado Bucles 4

---
+++ Bucles/bucles4.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+$num="8";
+$multiplicador="1";
+
+while($multiplicador <= 10){
+    $resultado = $num*$multiplicador;
+    echo "$num x $multiplicador = $resultado </br>";
+    $multiplicador++;
+}
+?>
+</BODY>
+</HTML>
