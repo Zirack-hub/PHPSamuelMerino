6ebf31de20bc065576e9aa3025f92d755a8ae005
Samuel <s.merino.prado@gmail.com>
2025-09-23 12:00:58 +0200

    Factorial

---
+++ Bucles/Bucles6.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD><TITLE> EJ6B – Factorial</TITLE></HEAD>
+<BODY>
+<?php
+$num="5";
+$factorial:"";
+for ($i=1; $i <= $num; $i++) { 
+    $factorial = $factorial+($num*$i);
+}
+?>
+</BODY>
+</HTML>
