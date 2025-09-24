f391c47dfabfd298584fe5941c6824508dc3b394
Samuel <s.merino.prado@gmail.com>
2025-09-24 10:56:26 +0200

    Ejercicio 6 Terminado

--- Bucles/Bucles6.php
+++ Bucles/Bucles6.php
@@ -2,11 +2,17 @@
 <HEAD><TITLE> EJ6B – Factorial</TITLE></HEAD>
 <BODY>
 <?php
-$num="5";
-$factorial:"";
-for ($i=1; $i <= $num; $i++) { 
-    $factorial = $factorial+($num*$i);
+$num=5;
+$factorial= 1;
+echo "$num!= ";
+for ($i=$num; $i >= 1; $i--) { 
+    echo "$i";
+    if ($i> 1) {
+        echo "x";
+    }
+    $factorial *=$i;
 }
+echo " =$factorial";
 ?>
 </BODY>
 </HTML>
