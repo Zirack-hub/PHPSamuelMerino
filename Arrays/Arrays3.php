6d77989c83f813cd16520ed3bc3cad9bccf11765
Samuel <s.merino.prado@gmail.com>
2025-09-25 09:37:53 +0200

    Arrays 3

---
+++ Arrays/Arrays3.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD><TITLE> EJ6B – Factorial</TITLE></HEAD>
+<BODY>
+<?php
+$binarios=[];
+$octales=[];
+
+for ($i=0; $i <20 ; $i++) { 
+    $binarios[$i]=decbin($i);
+    $octales[$i]=decoct($i);
+}
+
+echo "<h2>Tabla de Impares</h2>";
+echo "<table border='1' cellpadding='6' cellspacing='0'>";
+echo "<tr><th>Indice</th><th>Binario</th><th>Octal</th></tr>";
+
+
+for ($i=0; $i <count($binarios) ; $i++) { 
+    echo "<tr>";
+    echo "<td>$i</td>";
+    echo "<td>$binarios[$i]</td>";
+    echo "<td>$octales[$i]</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
+</BODY>
+</HTML>
