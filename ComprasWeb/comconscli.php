56a53c6c251fd2b0d7706482e754407528c6ade9
Zirack <s.merino.prado@gmail.com>
2026-01-07 18:58:25 +0100

    subo ultimos cambios

--- ComprasWeb/comconscli.php
+++ ComprasWeb/comconscli.php
@@ -7,8 +7,48 @@
     <title>Aprovisionar</title>
 </head>
 <body>
-    <h1>Aprovisionar Productos</h1>
-        <p>Aprovisionar Productos<p>
-        
+    <h1>Registro de Clientes</h1>
+        <p>Registro de Clientes<p>
+        <div class="card-body">
+        <form name="alta" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <?php
+            $cookie_name = "usuariocompras";
+            require_once ("./funciones/funciones.php");
+            require_once ("./funciones/fbd.php");
+
+            $conn = openBD('comprasweb');
+            $usuario = selectASSOC("SELECT NOMBRE, APELLIDO, NIF FROM CLIENTE WHERE NIF = '" . $_COOKIE['usuariocompras'] . "'",$conn);
+
+            $cliente = $usuario[0];
+            echo "Hola {$cliente['NOMBRE']} {$cliente['APELLIDO']}<br>";
+
+
+
+            echo "REGISTRO:<br>";
+            $compras = selectASSOC("SELECT NOMBRE, FECHA_COMPRA, UNIDADES FROM COMPRA C, PRODUCTO P WHERE NIF = '" . $cliente['NIF'] . "' AND C.ID_PRODUCTO = P.ID_PRODUCTO ORDER BY FECHA_COMPRA DESC",$conn);
+            foreach ($compras as $compra){
+                echo "{$compra['NOMBRE']} --- {$compra['UNIDADES']} --- {$compra['FECHA_COMPRA']}<br>";
+            }
+            echo "<a href='./comlogcli.php'>VOLVER AL REGISTRO</a><br>";
+            ?>
+            
+            
+            
+            
+        </form>
 </body>
 </html>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        closeBD($conn);
+        }
+    
+
+    
+
+
+
+
+?>
+    
--- ComprasWeb/comprocli.php
+++ ComprasWeb/comprocli.php
@@ -1,3 +1,11 @@
+<?php
+    session_start();
+    require_once ("./funciones/funciones.php");
+    require_once ("./funciones/fbd.php");
+    require_once ("./funciones/fcompras.php");
+    iniciarCarrito();
+    
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,21 +15,62 @@
     <title>Aprovisionar</title>
 </head>
 <body>
-    <h1>Aprovisionar Productos</h1>
-        <p>Aprovisionar Productos<p>
+    <h1>Portal de compras</h1>
+        <p>Portal de compras</p>
         <div class="card-body">
         <form name="alta" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <?php
-            require_once ("./funciones/funciones.php");
-            require_once ("./funciones/fbd.php");
+            
 
-            $conn = $conn = openBD('comprasweb');
+            $conn = openBD('comprasweb');
             
             $resultado = selectASSOC("SELECT ID_PRODUCTO, NOMBRE FROM PRODUCTO", $conn);
             mostrarOpciones("ID_PRODUCTO", $resultado, "PRODUCTO", "NOMBRE");
             
             ?>
+            Cantidad de productos <input type="text" name="cantidad">
             <input type="submit" name="submit" value="Añadir">
+            <input type="submit" name="submit" value="Eliminar">
+            <input type="submit" name="submit" value="Finalizar Compra">
+
         </form>
+        <p>Carrito</p>
+        <?php
+            foreach ($_SESSION['carrito'] as $producto => $cantidad){
+                $nombre = selectCOL("SELECT NOMBRE FROM PRODUCTO WHERE ID_PRODUCTO = '$producto'", $conn);
+                echo("$nombre.......... cantidad: $cantidad <br>");
+            }
+
+        ?>
+
+
+
 </body>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $eleccion = limpiar_campos($_POST["submit"]);
+    $producto = limpiar_campos($_POST["PRODUCTO"]);
+    $cantidad = limpiar_campos($_POST["cantidad"]);
+    if ($eleccion == "Añadir") {
+        agregarProducto($producto, $cantidad);
+        var_dump($_SESSION);
+        var_dump($_COOKIE['usuariocompras']);
+    }elseif ($eleccion == "Eliminar") {
+        eliminarProducto($producto, $cantidad);
+    }elseif ($eleccion == "Finalizar Compra") {
+        $flag = comprobarCarrito($conn);
+        if ($flag == true){
+            realizarCompra($conn);
+            session_destroy();
+            closeBD($conn);
+        }else{
+            echo ("NO SE PUEDE REALIZAR LA COMPRA.");
+        }
+
+    }
+
+    }
+
+?>
 </html>
