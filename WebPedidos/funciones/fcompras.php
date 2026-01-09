fa87a81c65dcd6c1963d3e735db03531756ae1cc
Samuel <s.merino.prado@gmail.com>
2026-01-09 13:01:12 +0100

    arreglo cosas y subo pedidos para casa

---
+++ WebPedidos/funciones/fcompras.php
@@ -0,0 +1,125 @@
+<?php
+require_once ("./funciones/funciones.php");
+require_once ("./funciones/fbd.php");
+
+
+function iniciarCarrito (){
+    if (!isset($_SESSION['carrito'])) {
+    $_SESSION['carrito'] = [];
+    }
+}
+
+function cerrarSesion ($cookie_name){
+    setcookie("$cookie_name", "", time() - 3600,"/");
+    header("Location: ./pe_login.php");
+    exit();
+}
+
+function agregarProducto($id, $cantidad) {
+
+    $cantidad = (int) $cantidad;
+
+    if ($cantidad <= 0) {
+        echo '<span style="color:red;">Introduce un número</span>';
+        return;
+    }
+
+    if (!isset($_SESSION['carrito'])) {
+        $_SESSION['carrito'] = [];
+    }
+
+    if (isset($_SESSION['carrito'][$id])) {
+        $_SESSION['carrito'][$id] += $cantidad;
+    } else {
+        $_SESSION['carrito'][$id] = $cantidad;
+    }
+}
+
+function eliminarProducto($id, $cantidad) {
+    $cantidad = (int) $cantidad;
+
+    if ($cantidad <= 0) {
+        echo '<span style="color:red;">Introduce un número válido</span>';
+        return;
+    }
+
+    if (!isset($_SESSION['carrito'])) {
+        $_SESSION['carrito'] = [];
+    }
+
+    if (isset($_SESSION['carrito'][$id])) {
+        // Restamos la cantidad
+        $_SESSION['carrito'][$id] -= $cantidad;
+
+        // Si llega a 0 o menos, eliminamos el producto
+        if ($_SESSION['carrito'][$id] <= 0) {
+            unset($_SESSION['carrito'][$id]);
+        }
+    }
+}
+
+
+function comprobarCarrito($conn){
+    $flag = true;
+    foreach ($_SESSION['carrito'] as $producto => $cantidad){
+        $cotejar =  selectCol("SELECT SUM(CANTIDAD) AS TOTAL FROM ALMACENA WHERE ID_PRODUCTO = '$producto'", $conn);
+        if ($cantidad > $cotejar){
+            $nombre = selectCol("SELECT NOMBRE FROM PRODUCTO WHERE ID_PRODUCTO = '$producto'", $conn);
+            echo ("La cantidad seleccionada del articulo $nombre no existe");
+            $flag = false;
+        }
+    }
+    return $flag;
+}
+
+function realizarCompra($conn){
+    foreach ($_SESSION['carrito'] as $producto => $cantidad){
+        $cotejar =  selectCol("SELECT CANTIDAD FROM ALMACENA WHERE ID_PRODUCTO = '$producto'", $conn);
+            $cant = $cantidad;
+            while ($cant > 0){
+                $cotejar =  selectCol("SELECT CANTIDAD FROM ALMACENA WHERE ID_PRODUCTO = '$producto' AND CANTIDAD > 0 ORDER BY NUM_ALMACEN LIMIT 1", $conn);
+                try{
+                    $conn->beginTransaction();
+                if ($cotejar < $cant){
+                    $stmt = $conn->prepare("UPDATE ALMACENA SET CANTIDAD = CANTIDAD - :cotejar WHERE ID_PRODUCTO = :producto AND CANTIDAD > 0 ORDER BY NUM_ALMACEN LIMIT 1");
+                    $stmt->bindParam(':producto', $producto);
+                    $stmt->bindParam(':cotejar', $cotejar);
+                }else{
+                    $stmt = $conn->prepare("UPDATE ALMACENA SET CANTIDAD = CANTIDAD - :cantidad WHERE ID_PRODUCTO = :producto AND CANTIDAD > 0 ORDER BY NUM_ALMACEN LIMIT 1");
+                    $stmt->bindParam(':producto', $producto);
+                    $stmt->bindParam(':cantidad', $cant);
+                }
+                $stmt->execute();
+                $conn->commit(); 
+                $cant = $cant - $cotejar;
+                }
+                 catch(PDOException $e)
+                    {
+                    if ($conn && $conn->inTransaction()) {
+                        $conn->rollback();
+                    }
+                    echo "Connection failed: " . $e->getMessage();
+                    echo "Código de error: " . $e->getCode() . "<br>";
+                    }
+            }
+            
+            try{
+            $fecha = date("Y-m-d H:i:s"); 
+            $stmt = $conn->prepare("INSERT INTO compra(NIF, ID_PRODUCTO, FECHA_COMPRA, UNIDADES) VALUES (:NIF,:ID_PRODUCTO,:FECHA_COMPRA,:UNIDADES)");
+            $stmt->bindParam(':NIF', $_COOKIE['usuariocompras']);
+            $stmt->bindParam(':ID_PRODUCTO', $producto);
+            $stmt->bindParam(':FECHA_COMPRA', $fecha);
+            $stmt->bindParam(':UNIDADES', $cantidad);
+            $stmt->execute();
+        }
+        catch(PDOException $e)
+           {
+            if ($conn && $conn->inTransaction()) {
+                $conn->rollback();
+            }
+            echo "Connection failed: " . $e->getMessage();
+            echo "Código de error: " . $e->getCode() . "<br>";
+           }
+    }
+}
+?>
---
+++ WebPedidos/pe_login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login cliente</title>
+</head>
+<body>
+    <h1>Login cliente</h1>
+		<p>Login clientes<p>
+		<div class="card-body">
+		<form name="alta" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <?php
+                $cookie_name = "usuariopedidos";
+                require_once("./funciones/funciones.php");
+                require_once("./funciones/fbd.php");
+                require_once("./funciones/fcompras.php");
+
+                $conn = openBD("webpedidos");
+
+                if(!isset($_COOKIE[$cookie_name])) {
+                ?>
+                    Usuario (CustomerNumber): <input type="text" name="usuario">
+                    contraseña (ContactLastname): <input type="text" name="clave">
+                    <input type="submit" name="submit" value="Iniciar Sesión">
+                <?php
+                } 
+                else {
+                    $name = selectCol("SELECT CUSTOMERNAME FROM CUSTOMERS WHERE CUSTOMERNUMBER = '$_COOKIE[$cookie_name]'",$conn);
+                    echo "Has iniciado sesión como $name<br>";
+                    echo "COMPRAS: <br>";
+                    echo "<a href='./pe_altaped.php'> Ver compras</a><br>";
+                    echo "HISTORIAL: <br>";
+                    echo "<a href='./pe_consped.php'> Ver historial</a><br>";
+                    echo "STOCK: <br>";
+                    echo "<a href='./pe_consprodstock.php'> Ver historial</a><br>";
+                    echo "STOCK LINEAS PRODUCCION: <br>";
+                    echo "<a href='./pe_constock.php'> Ver historial</a><br>";
+                    echo "</br></br><input type='submit' name='submit' value='Cerrar Sesión'>";
+
+                }
+            ?>
+        </form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $eleccion = limpiar_campos($_POST["submit"]);
+
+        if ($eleccion == "Iniciar Sesión") {
+            $usuario = limpiar_campos($_POST["usuario"]);
+            $clave = limpiar_campos($_POST["clave"]);
+
+            $registro = selectCOL("SELECT CUSTOMERNUMBER FROM CUSTOMERS WHERE CONTACTLASTNAME = '$clave' AND CUSTOMERNUMBER = '$usuario'",$conn);
+            
+            if ($registro != null) {
+                $name = selectCol("SELECT CUSTOMERNAME FROM CUSTOMERS WHERE CUSTOMERNUMBER = '$usuario'",$conn);
+                setcookie($cookie_name, $registro, time() + (86400 * 30), "/");
+                echo "Has iniciado sesión como $name <br>";
+                echo "COMPRAS: <br>";
+                echo "<a href='./comprocli.php'> Ver compras</a><br>";
+                echo "HISTORIA: <br>";
+                echo "<a href='./comconscli.php'> Ver compras</a><br>";
+                
+                echo '
+                        <form name="alta" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" method="post">
+                            <br><br>
+                            <input type="submit" name="submit" value="Cerrar Sesión">
+                        </form>';
+
+                
+            }
+            else {
+                echo "El usuario y la contraseña no coinciden";
+            }
+        }
+        elseif ($eleccion == "Cerrar Sesión") {
+            cerrarSesion($cookie_name);
+        }
+}
+?>
+</body>
+</html>
